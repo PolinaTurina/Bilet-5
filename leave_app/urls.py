8832ecf0c275d0953fc19c3dcae720641ffe6b1d
PolinaTurina <polinaturina87@gmail.com>
2023-06-30 23:19:25 +0500

    навигация

--- leave_app/urls.py
+++ leave_app/urls.py
@@ -5,9 +5,9 @@ from leave_app.views import *
 app_name = 'leave_app'
 
 urlpatterns = [
-    path('otpusk/', OtpuskListView.as_view()),
-    path('otgul/', OtgulListView.as_view()),
-    path('create/', OtpuskCreateView.as_view()),
-    path('create2/', OtgulCreateView.as_view()),
+    path('otpusk/', OtpuskListView.as_view(), name='otpusk_list'),
+    path('otgul/', OtgulListView.as_view(), name='otgul_list'),
+    path('create/', OtpuskCreateView.as_view(), name='form_create'),
+    path('create2/', OtgulCreateView.as_view(), name='form_create_2'),
     path('otkaz/<int:app_id>/', otkaz_view),
 ]
