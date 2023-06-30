397c7f7bdf0982e0b29b66b38fc6fa1f39807f0e
PolinaTurina <polinaturina87@gmail.com>
2023-06-30 13:56:48 +0500

    модель + админка

--- leave_app/admin.py
+++ leave_app/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from leave_app.models import *
 
-# Register your models here.
+@admin.register(Otpusk)
+class OtpuskAdmin(admin.ModelAdmin):
+    redonly_fields = ['created_date', 'edit_date'] #поля для чтения
+    list_display = ['name', 'start_date', 'end_date', 'odobreno',
+                    'prosmotreno', 'created_date', 'edit_date'] #отображаемые поля
+    search_fields = ['name', 'note'] #поиск по полю
+    list_filter = ['odobreno', 'prosmotreno', 'start_date',
+                    'end_date', 'created_date', 'edit_date'] #сортировка
+
+
+@admin.register(Otgul)
+class OtgulAdmin(admin.ModelAdmin):
+    redonly_fields = ['created_date', 'edit_date'] #поля для чтения
+    list_display = ['name', 'date', 'odobreno',
+                    'prosmotreno', 'created_date', 'edit_date'] #отображаемые поля
+    search_fields = ['name', 'note'] #поиск по полю
+    list_filter = ['odobreno', 'prosmotreno', 'date',
+                    'created_date', 'edit_date'] #сортировка
--- leave_app/models.py
+++ leave_app/models.py
@@ -1,1 +1,39 @@
 from django.db import models
+
+
+#Заявка на отпуск и отгул
+class Base(models.Model):
+    note = models.TextField(blank=False, verbose_name='Примечание')
+    name = models.CharField(max_length=96, verbose_name='ФИО')
+    odobreno = models.BooleanField(default=False, blank=False, verbose_name='Заявка одобрена')
+    prosmotreno = models.BooleanField(default=False, blank=False, verbose_name='Заявка просмотрена')
+    created_date=models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    edit_date=models.DateTimeField(auto_now=True, verbose_name='Дата редактирования')
+
+    class Meta:
+        abstract = True
+
+#Заявка на отпуск наслудется от Base
+class Otpusk(Base):
+    start_date = models.DateField(blank=False, verbose_name='Начало отпуска')
+    end_date = models.DateField(blank=False, verbose_name='Конец отпуска')
+
+    class Meta:
+        verbose_name = 'Заявка на отпуск'
+        verbose_name_plural = 'Заявки на отпуск'
+        ordering = ['created_date', 'edit_date']
+
+    def __str__(self):
+        return f'Заявка на отпуск № {self.id}'
+
+#Заявка на отгул наслудется от Base
+class Otgul(Base):
+    date = models.DateField(blank=False, verbose_name='Дата')
+
+    class Meta:
+        verbose_name = 'Заявка на отгул'
+        verbose_name_plural = 'Заявки на отгул'
+        ordering = ['created_date', 'edit_date']
+
+    def __str__(self):
+        return f'Заявка на отгул № {self.id}'
