a005481f2268a85d25b5359ffb9cfad6042abcdd
PolinaTurina <polinaturina87@gmail.com>
2023-06-30 16:33:06 +0500

    вывели заявки на отпуск

---
+++ leave_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from leave_app.views import *
+
+
+app_name = 'leave_app'
+
+urlpatterns = [
+    path('otpusk/', OtpuskListView.as_view()),
+]
--- leave_app/views.py
+++ leave_app/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from leave_app.models import *
 
 
-
+class OtpuskListView(ListView):
+    model = Otpusk
+    template_name = 'leave_app/otpusk_list.html'
+    context_object_name = 'otpusk_list'
